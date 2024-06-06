ddf9c7f4d30311828aaf556b92c57626034bc7a8
aliashkov <art1699584@gmail.com>
2024-06-06 11:45:49 +0300

    add: methods for recording

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import { BasicPitch } from '@spotify/basic-pitch';
 import {
   addPitchBendsToNoteEvents,
@@ -8,17 +8,53 @@ import {
 import { downloadMidiFile } from './utils/downloadMidiFile';
 import { generateFileData } from './utils/generateFileData';
 import { decodeDataToAudioBuffer } from './utils/decodeDataToAudioBuffer';
+import Recorder from 'recorder-js';
 
 function App() {
   const [fileName, setFileName] = useState('');
+  const [isRecording, setIsRecording] = useState(false);
+  const [arrayFileBuffer, setArrayFileBuffer] = useState([])
+  const recorderRef = useRef(null);
 
-  const generateMidiFile = async (event) => {
+  const loadFile = async (event) => {
     const file = event.target.files[0];
+    console.log(file)
     if (file) {
       setFileName(file.name);
       const arrayBuffer = await file.arrayBuffer();
-      let audioBuffer = await decodeDataToAudioBuffer(arrayBuffer)
+      setArrayFileBuffer(arrayBuffer)
+    }
+  };
 
+  const startRecording = async () => {
+    if (!recorderRef.current) {
+      const audioContext = new AudioContext();
+      const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+      const recorder = new Recorder(audioContext);
+      recorder.init(stream);
+      recorderRef.current = recorder;
+    }
+
+    recorderRef.current.start();
+    setIsRecording(true);
+  };
+
+  const stopRecording = async () => {
+    const { blob } = await recorderRef.current.stop();
+    setFileName(`recording-${new Date().toISOString().slice(0, 10)}.mp3`);
+  
+    var file = new File([blob], "name");
+    const arrayBuffer = await file.arrayBuffer();
+
+    setArrayFileBuffer(arrayBuffer);
+    setIsRecording(false);
+  };
+
+  const generateMidiFile = async () => {
+    console.log(arrayFileBuffer);
+    let audioBuffer = await decodeDataToAudioBuffer(arrayFileBuffer);
+
+    if (audioBuffer) {
       const frames = [];
       const onsets = [];
       const contours = [];
@@ -47,22 +83,39 @@ function App() {
 
       const midiData = generateFileData(notes);
 
-      downloadMidiFile(midiData, 'generated-midi-file.mid');
+      const currentDate = new Date().toISOString().slice(0, 10);
+      const midiFileNameWithDate = `generated-midi-file-${currentDate}.mid`;
+
+      downloadMidiFile(midiData, midiFileNameWithDate);
+    } else {
+      console.error('Error: audioBuffer is undefined or null');
     }
   };
 
+
   return (
     <div className="App">
-      <input 
-        type="file" 
-        onChange={generateMidiFile} 
-        style={{ display: 'none' }} 
+      <input
+        type="file"
+        accept=".wav,.mp3,.ogg,.flac"
+        onChange={loadFile}
+        style={{ display: 'none' }}
         id="fileInput"
       />
       <button onClick={() => document.getElementById('fileInput').click()}>
         Load File
       </button>
       {fileName && <p>Loaded file: {fileName}</p>}
+
+      <button onClick={isRecording ? stopRecording : startRecording}>
+        {isRecording ? 'Stop Recording' : 'Start Recording'}
+      </button>
+
+      {fileName && !isRecording ? (
+        <button onClick={generateMidiFile}>
+          Generate Midi File
+        </button>
+      ) : <></>}
     </div>
   );
 }
